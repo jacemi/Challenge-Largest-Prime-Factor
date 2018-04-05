d65f59b871753de83c3efb60ac24644968317c44
Jace Mitsuka <PJace@BigMac.local>
2018-04-05 10:34:55 -1000

    version 1

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,31 @@
 exports.largestPrimeFactor = function(n){
   var primeNumber = 0;
-  // do your work here
+  var factorArr = [];
+  for(let i = 0; i <= n; i++){
+    if(n%[i] === 0){
+      factorArr.push([i]);
+    }
+  }
 
   return primeNumber;
 };
+
+
+function primeFactor(n) {
+  let primeNumber = 0;
+  let factorArr = [];
+  let primeFactorArr = [];
+  for (let i = 0; i <= n; i++) {
+    if (n % i === 0) {
+      factorArr.push(i);
+    }
+    
+  }
+  for (let x = 0; x <= factorArr.length; x++) {
+    for (let y = 0; y <= factorArr.pop(); y++){
+    if (factorArr[x] % y !== 0) {
+      primeFactorArr.push(factorArr[x]);
+    }
+  }
+  } return primeFactorArr;
+}
